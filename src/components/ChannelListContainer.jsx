89d84a0e083f19973bf29a6c6b3d85ff2dece7e2
JeimarD <jeimar1512@gmail.com>
2022-07-04 22:39:33 -0400

    Finished the first day of chat project, continue with server index.js

---
+++ src/components/ChannelListContainer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import GameIcon from "../assets/logo.svg";
+import LogoutIcon from "../assets/logout.png";
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={GameIcon} alt="Games" width="30" />
+      </div>
+    </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon2__inner">
+        <img src={LogoutIcon} alt="Logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
+
+const CompanyHeader = () => (
+  <div className="channel-list__header">
+    <p className="channel-list__header__text">Gaming Pager</p>
+  </div>
+);
+
+const ChannelListContainer = () => {
+  return (
+    <>
+      <SideBar />
+      <div className="channel-list__list__wrapper">
+        <CompanyHeader />
+        <ChannelSearch />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => <TeamChannelList {...listProps} type="team" />}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="team" />
+          )}
+        />
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => {}}
+          List={(listProps) => (
+            <TeamChannelList {...listProps} type="messaging" />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview {...previewProps} type="messaging" />
+          )}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ChannelListContainer;
